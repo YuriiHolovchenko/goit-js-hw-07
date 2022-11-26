e2198a135efedaffc4f492d9b4774c9bf08de621
Yurii Holovchenko <yougo2034@gmail.com>
2022-11-26 10:58:06 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryCards = document.querySelector('div.gallery');
+const cards = createGalleryCards(galleryItems);
+
+galleryCards.insertAdjacentHTML('beforeend', cards);
+
+galleryCards.addEventListener('click', onCardClick);
+
+
+
+function createGalleryCards() {
+return galleryItems.map(({preview, original, description}) => {
+    return`
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+})
+.join('');
+};
+function onCardClick(event) {
+    // console.log(event);
+    console.log(event.target.dataset.source);
+    // console.log(event.currentTarget)
+
+    // const isCard = evt.target.classList.contains('gallery_image');
+    // if(!isCard) {
+    //     return;
+    // }
+    // console.log(evt.);
+}
+
